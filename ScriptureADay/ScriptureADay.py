d580b88db682ba212c30d43d20f6542811a5d627
Jordan G <jorg2332@gmail.com>
2018-07-25 22:16:36 -0600

    Displays 1 Nephi 3:7

--- ScriptureADay/ScriptureADay.py
+++ ScriptureADay/ScriptureADay.py
@@ -1,1 +1,13 @@
+import json
 
+try:
+    with open('Scriptures/book-of-mormon.json', 'r') as file:
+        bom = json.load(file) 
+except ValueError:
+    print("Error loading JSON")
+
+#Prints 1 Nephi 3:7
+book = bom["books"][0]
+chapter = book["chapters"][2] 
+verse = ["verses"][6]["text"]
+print(verse)
