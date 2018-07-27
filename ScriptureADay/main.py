cf5685f6e411c65d2b19e88f6669493f9779c02d
Jordan G <jorg2332@gmail.com>
2018-07-27 12:17:24 -0600

    retrieves random scripture from db

--- ScriptureADay/main.py
+++ ScriptureADay/main.py
@@ -3,6 +3,27 @@ import mysql.connector
 import databaseconfig as dbconfig
 from win10toast import ToastNotifier
 
+#def DisplayBoM(bookNum, chapterNum, verseNum):
+#    #Prints 1 Nephi 3:7
+#    book = bom["books"][bookNum]
+#    chapter = book["chapters"][chapterNum - 1] 
+#    verse = chapter["verses"][verseNum - 1]["text"]
+#    notification = ToastNotifier()
+#    notification.show_toast("1 Nephi 3:7", verse)
+
+def GetRandomScriptureFromDB(dbcursor):
+    sql = "SELECT * FROM scriptures ORDER BY RAND() LIMIT 1"
+    dbcursor.execute(sql)
+    result = dbcursor.fetchone()
+    return result
+
+def GetBookName(bookID, dbcursor):
+    sql = "SELECT bookName FROM books WHERE bookID = " + str(bookID)
+    dbcursor.execute(sql)
+    result = dbcursor.fetchone()
+    bookName = result[0]
+    return bookName
+
 try:
     connection = mysql.connector.connect(
         user=dbconfig.mysql['user'], password=dbconfig.mysql['password'], 
@@ -10,7 +31,10 @@ try:
 except InterfaceError:
     print("Could not connect to database")
 
-notification = ToastNotifier()
+dbcursor = connection.cursor()
+randomScripture = GetRandomScriptureFromDB(dbcursor)
+bookID = randomScripture[0]
+bookName = GetBookName(bookID, dbcursor)
 
 try:
     with open('Scriptures/book-of-mormon.json', 'r') as file:
@@ -18,13 +42,5 @@ try:
 except ValueError:
     print("Error loading JSON")
 
-def DisplayBoM(bookNum, chapterNum, verseNum):
-    #Prints 1 Nephi 3:7
-    book = bom["books"][bookNum]
-    chapter = book["chapters"][chapterNum - 1] 
-    verse = chapter["verses"][verseNum - 1]["text"]
-    notification.show_toast("1 Nephi 3:7", verse)
-
-DisplayBoM(0, 3, 7)
 
 connection.close()
